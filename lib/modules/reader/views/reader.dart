897d8ed92227a0fd698a203a0a0932915cc62f57
Igbana <igbanaaondosoo13@gmail.com>
2025-10-08 12:44:38 +0100

    fix: Sorted the problem particular for Web with Syncfusion

--- lib/modules/reader/views/reader.dart
+++ lib/modules/reader/views/reader.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:visually_impaired/modules/reader/controllers/reader_controller.dart';
 
 class Reader extends StatelessWidget {
   const Reader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SfPdfViewer.asset('doc/CIT101.pdf'),
-      // child: SfPdfViewer.network(
-      //   'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
-      // ),
+    final controller = Get.find<ReaderController>();
+    return Column(
+      children: [
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                controller.pageController.previousPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.ease,
+                );
+              },
+              icon: const Icon(Icons.arrow_back),
+            ),
+            const SizedBox(width: 12),
+            const Text("CIT101.pdf"),
+          ],
+        ),
+        const SizedBox(height: 12),
+        Expanded(child: SfPdfViewer.asset('doc/CIT101.pdf')),
+      ],
     );
   }
 }
